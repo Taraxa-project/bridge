71c979655ba20819e952141c1273ee71dfcbcbfa
VargaElod23 <vargaelod23@gmail.com>
2024-06-11 20:36:42 +0200

    adheres deployment scripts to new setup

--- script/Eth.deploy.s.sol
+++ script/Eth.deploy.s.sol
@@ -1,119 +1,118 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.17;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
 
-// import "forge-std/console.sol";
-// import {Script} from "forge-std/Script.sol";
-// import {Upgrades, Options} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import "forge-std/console.sol";
+import {Script} from "forge-std/Script.sol";
+import {Upgrades, Options} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
-// import "../src/lib/Constants.sol";
-// import {EthBridge} from "../src/eth/EthBridge.sol";
-// import {TaraClient, PillarBlock} from "../src/eth/TaraClient.sol";
-// import {TestERC20} from "../src/lib/TestERC20.sol";
-// import {IBridgeLightClient} from "../src/lib/IBridgeLightClient.sol";
-// import {ERC20MintingConnector} from "../src/connectors/ERC20MintingConnector.sol";
-// import {IBridgeConnector} from "../src/connectors/IBridgeConnector.sol";
-// import {NativeConnector} from "../src/connectors/NativeConnector.sol";
+import {Constants} from "../src/lib/Constants.sol";
+import {EthBridge} from "../src/eth/EthBridge.sol";
+import {TaraClient, PillarBlock} from "../src/eth/TaraClient.sol";
+import {TestERC20} from "../src/lib/TestERC20.sol";
+import {IBridgeLightClient} from "../src/lib/IBridgeLightClient.sol";
+import {ERC20MintingConnector} from "../src/connectors/ERC20MintingConnector.sol";
+import {IBridgeConnector} from "../src/connectors/IBridgeConnector.sol";
+import {NativeConnector} from "../src/connectors/NativeConnector.sol";
 
-// contract EthDeployer is Script {
-//     address public deployerAddress;
-//     address public taraAddressOnEth;
-//     address public ethAddressOnTara;
-//     uint256 public deployerPrivateKey;
-//     uint256 public finalizationInterval = 100;
+contract EthDeployer is Script {
+    address public deployerAddress;
+    address public taraAddressOnEth;
+    address public ethAddressOnTara;
+    uint256 public deployerPrivateKey;
 
-//     function setUp() public {
-//         deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-//         deployerAddress = vm.envAddress("DEPLOYMENT_ADDRESS");
-//         console.log("Deployer address: %s", deployerAddress);
+    uint256 constant FINALIZATION_INTERVAL = 100;
+    uint256 constant FEE_MULTIPLIER_ETH = 105;
+    uint256 constant REGISTRATION_FEE_ETH = 2 ether;
+    uint256 constant SETTLEMENT_FEE_ETH = 500 gwei;
 
-//         if (vm.envUint("PRIVATE_KEY") == 0) {
-//             console.log("Skipping deployment because PRIVATE_KEY is not set");
-//             return;
-//         }
-//         // check if balance is at least 2 * MINIMUM_CONNECTOR_DEPOSIT
-//         if (address(deployerAddress).balance < 2 * Constants.MINIMUM_CONNECTOR_DEPOSIT) {
-//             console.log("Skipping deployment because balance is less than 2 * MINIMUM_CONNECTOR_DEPOSIT");
-//             return;
-//         }
+    function setUp() public {
+        deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        deployerAddress = vm.envAddress("DEPLOYMENT_ADDRESS");
+        console.log("Deployer address: %s", deployerAddress);
 
-//         taraAddressOnEth = vm.envAddress("TARA_ADDRESS_ON_ETH");
-//         console.log("TARA_ADDRESS_ON_ETH: %s", taraAddressOnEth);
-//         ethAddressOnTara = vm.envAddress("ETH_ADDRESS_ON_TARA");
-//         console.log("ETH_ADDRESS_ON_TARA: %s", ethAddressOnTara);
-//     }
+        if (vm.envUint("PRIVATE_KEY") == 0) {
+            console.log("Skipping deployment because PRIVATE_KEY is not set");
+            return;
+        }
 
-//     function run() public {
-//         vm.startBroadcast(deployerPrivateKey);
-//         Options memory opts;
-//         opts.defender.useDefenderDeploy = false;
+        if (address(deployerAddress).balance < (2 * REGISTRATION_FEE_ETH)) {
+            console.log("Skipping deployment because balance is less than 2 *  REGISTRATION_FEE_ETH");
+            return;
+        }
 
-//         address taraClientProxy = Upgrades.deployUUPSProxy(
-//             "TaraClient.sol", abi.encodeCall(TaraClient.initialize, (finalizationInterval)), opts
-//         );
+        taraAddressOnEth = vm.envAddress("TARA_ADDRESS_ON_ETH");
+        console.log("TARA_ADDRESS_ON_ETH: %s", taraAddressOnEth);
+        ethAddressOnTara = vm.envAddress("ETH_ADDRESS_ON_TARA");
+        console.log("ETH_ADDRESS_ON_TARA: %s", ethAddressOnTara);
+    }
 
-//         console.log("TaraClient.sol proxy address: %s", taraClientProxy);
-//         console.log("TaraClient.sol implementation address: %s", Upgrades.getImplementationAddress(taraClientProxy));
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
+        Options memory opts;
+        opts.defender.useDefenderDeploy = false;
 
-//         address ethBridgeProxy = Upgrades.deployUUPSProxy(
-//             "EthBridge.sol",
-//             abi.encodeCall(
-//                 EthBridge.initialize,
-//                 (IBridgeLightClient(taraClientProxy), finalizationInterval)
-//             ),
-//             opts
-//         );
+        address taraClientProxy = Upgrades.deployUUPSProxy(
+            "TaraClient.sol", abi.encodeCall(TaraClient.initialize, (FINALIZATION_INTERVAL)), opts
+        );
 
-//         console.log("EthBridge.sol proxy address: %s", ethBridgeProxy);
-//         console.log("EthBridge.sol implementation address: %s", Upgrades.getImplementationAddress(ethBridgeProxy));
+        console.log("TaraClient.sol proxy address: %s", taraClientProxy);
+        console.log("TaraClient.sol implementation address: %s", Upgrades.getImplementationAddress(taraClientProxy));
 
-//         address mintingConnectorProxy = Upgrades.deployUUPSProxy(
-//             "ERC20MintingConnector.sol",
-//             abi.encodeCall(
-//                 ERC20MintingConnector.initialize,
-//                 (address(ethBridgeProxy), TestERC20(taraAddressOnEth), Constants.NATIVE_TOKEN_ADDRESS)
-//             ),
-//             opts
-//         );
+        address ethBridgeProxy = Upgrades.deployUUPSProxy(
+            "EthBridge.sol",
+            abi.encodeCall(
+                EthBridge.initialize,
+                (
+                    IBridgeLightClient(taraClientProxy),
+                    FINALIZATION_INTERVAL,
+                    FEE_MULTIPLIER_ETH,
+                    REGISTRATION_FEE_ETH,
+                    SETTLEMENT_FEE_ETH
+                )
+            ),
+            opts
+        );
 
-//         console.log("ERC20MintingConnector.sol proxy address: %s", mintingConnectorProxy);
-//         console.log(
-//             "ERC20MintingConnector.sol implementation address: %s",
-//             Upgrades.getImplementationAddress(mintingConnectorProxy)
-//         );
+        EthBridge ethBridge = EthBridge(payable(ethBridgeProxy));
 
-//         // Fund the MintingConnector with 2 ETH
-//         (bool success,) = payable(mintingConnectorProxy).call{value: Constants.MINIMUM_CONNECTOR_DEPOSIT}("");
-//         if (!success) {
-//             revert("Failed to fund the MintingConnector");
-//         }
+        console.log("EthBridge.sol proxy address: %s", ethBridgeProxy);
+        console.log("EthBridge.sol implementation address: %s", Upgrades.getImplementationAddress(ethBridgeProxy));
 
-//         address owner = TestERC20(taraAddressOnEth).owner();
-//         console.log("Owner: %s", owner);
+        address mintingConnectorProxy = Upgrades.deployUUPSProxy(
+            "ERC20MintingConnector.sol",
+            abi.encodeCall(
+                ERC20MintingConnector.initialize,
+                (ethBridge, TestERC20(taraAddressOnEth), Constants.NATIVE_TOKEN_ADDRESS)
+            ),
+            opts
+        );
 
-//         // give ownership of erc20 to the connector
-//         TestERC20(taraAddressOnEth).transferOwnership(mintingConnectorProxy);
+        console.log("ERC20MintingConnector.sol proxy address: %s", mintingConnectorProxy);
+        console.log(
+            "ERC20MintingConnector.sol implementation address: %s",
+            Upgrades.getImplementationAddress(mintingConnectorProxy)
+        );
 
-//         // Add the connector to the bridge
-//         EthBridge bridge = EthBridge(ethBridgeProxy);
-//         bridge.registerContract(IBridgeConnector(mintingConnectorProxy));
+        address owner = TestERC20(taraAddressOnEth).owner();
+        console.log("Owner: %s", owner);
 
-//         // Instantiate and register the NativeConnector
-//         address nativeConnectorProxy = Upgrades.deployUUPSProxy(
-//             "NativeConnector.sol", abi.encodeCall(NativeConnector.initialize, (address(bridge), ethAddressOnTara)), opts
-//         );
+        // give ownership of erc20 to the connector
+        TestERC20(taraAddressOnEth).transferOwnership(mintingConnectorProxy);
 
-//         // Fund the MintingConnector with 2 ETH
-//         (bool success2,) = payable(nativeConnectorProxy).call{value: Constants.MINIMUM_CONNECTOR_DEPOSIT}("");
-//         if (!success2) {
-//             revert("Failed to fund the NativeConnector");
-//         }
+        // Add the connector to the bridge
+        ethBridge.registerContract{value: REGISTRATION_FEE_ETH}(IBridgeConnector(mintingConnectorProxy));
 
-//         bridge.registerContract(IBridgeConnector(nativeConnectorProxy));
-//         console.log("NativeConnector.sol proxy address: %s", nativeConnectorProxy);
-//         console.log(
-//             "NativeConnector.sol implementation address: %s", Upgrades.getImplementationAddress(nativeConnectorProxy)
-//         );
+        // Instantiate and register the NativeConnector
+        address nativeConnectorProxy = Upgrades.deployUUPSProxy(
+            "NativeConnector.sol", abi.encodeCall(NativeConnector.initialize, (ethBridge, ethAddressOnTara)), opts
+        );
 
-//         vm.stopBroadcast();
-//     }
-// }
+        ethBridge.registerContract{value: REGISTRATION_FEE_ETH}(IBridgeConnector(nativeConnectorProxy));
+        console.log("NativeConnector.sol proxy address: %s", nativeConnectorProxy);
+        console.log(
+            "NativeConnector.sol implementation address: %s", Upgrades.getImplementationAddress(nativeConnectorProxy)
+        );
+
+        vm.stopBroadcast();
+    }
+}
--- script/Tara.deploy.s.sol
+++ script/Tara.deploy.s.sol
@@ -1,176 +1,185 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.17;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
 
-// import "forge-std/console.sol";
-// import "beacon-light-client/src/BeaconLightClient.sol";
-// import "beacon-light-client/src/BeaconChain.sol";
+import "forge-std/console.sol";
+import "beacon-light-client/src/BeaconLightClient.sol";
+import "beacon-light-client/src/BeaconChain.sol";
 
-// import "forge-std/console.sol";
-// import {Script} from "forge-std/Script.sol";
-// // import {Defender, ApprovalProcessResponse} from "openzeppelin-foundry-upgrades/Defender.sol";
-// import {Upgrades, Options} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import "forge-std/console.sol";
+import {Script} from "forge-std/Script.sol";
+// import {Defender, ApprovalProcessResponse} from "openzeppelin-foundry-upgrades/Defender.sol";
+import {Upgrades, Options} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
-// import "../src/lib/Constants.sol";
-// import {EthBridge} from "../src/eth/EthBridge.sol";
-// import {TaraClient, PillarBlock} from "../src/eth/TaraClient.sol";
-// import {TestERC20} from "../src/lib/TestERC20.sol";
-// import {IBridgeLightClient} from "../src/lib/IBridgeLightClient.sol";
-// import {EthClient} from "../src/tara/EthClient.sol";
-// import {TaraBridge} from "../src/tara/TaraBridge.sol";
-// import {NativeConnector} from "../src/connectors/NativeConnector.sol";
-// import {IBridgeConnector} from "../src/connectors/IBridgeConnector.sol";
-// import {ERC20MintingConnector} from "../src/connectors/ERC20MintingConnector.sol";
+import "../src/lib/Constants.sol";
+import {EthBridge} from "../src/eth/EthBridge.sol";
+import {TaraClient, PillarBlock} from "../src/eth/TaraClient.sol";
+import {TestERC20} from "../src/lib/TestERC20.sol";
+import {IBridgeLightClient} from "../src/lib/IBridgeLightClient.sol";
+import {EthClient} from "../src/tara/EthClient.sol";
+import {TaraBridge} from "../src/tara/TaraBridge.sol";
+import {NativeConnector} from "../src/connectors/NativeConnector.sol";
+import {IBridgeConnector} from "../src/connectors/IBridgeConnector.sol";
+import {ERC20MintingConnector} from "../src/connectors/ERC20MintingConnector.sol";
 
-// bytes32 constant GENERAL_BRIDGE_ROOT_KEY = 0x0000000000000000000000000000000000000000000000000000000000000008;
+bytes32 constant GENERAL_BRIDGE_ROOT_KEY = 0x0000000000000000000000000000000000000000000000000000000000000008;
 
-// contract TaraDeployer is Script {
-//     using Bytes for bytes;
+contract TaraDeployer is Script {
+    using Bytes for bytes;
 
-//     uint256 deployerPrivateKey;
-//     address deployerAddress;
-//     address taraAddressOnEth;
-//     address ethAddressOnTara;
+    uint256 deployerPrivateKey;
+    address deployerAddress;
+    address taraAddressOnEth;
+    address ethAddressOnTara;
 
-//     BeaconLightClient beaconClient;
+    BeaconLightClient beaconClient;
 
-//     function setUp() public {
-//         deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-//         deployerAddress = vm.envAddress("DEPLOYMENT_ADDRESS");
-//         console.log("Deployer address: %s", deployerAddress);
+    uint256 constant FINALIZATION_INTERVAL = 100;
+    uint256 constant FEE_MULTIPLIER_TARA = 100;
+    uint256 constant REGISTRATION_FEE_TARA = 950432 ether;
+    uint256 constant SETTLEMENT_FEE_TARA = 50000 ether;
 
-//         if (vm.envUint("PRIVATE_KEY") == 0) {
-//             console.log("Skipping deployment because PRIVATE_KEY is not set");
-//             return;
-//         }
-//         // check if balance is at least 2 * MINIMUM_CONNECTOR_DEPOSIT
-//         if (address(deployerAddress).balance < 2 * Constants.MINIMUM_CONNECTOR_DEPOSIT) {
-//             console.log("Skipping deployment because balance is less than 2 * MINIMUM_CONNECTOR_DEPOSIT");
-//             return;
-//         }
+    function setUp() public {
+        deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        deployerAddress = vm.envAddress("DEPLOYMENT_ADDRESS");
+        console.log("Deployer address: %s", deployerAddress);
 
-//         taraAddressOnEth = vm.envAddress("TARA_ADDRESS_ON_ETH");
-//         console.log("TARA_ADDRESS_ON_ETH: %s", taraAddressOnEth);
-//         ethAddressOnTara = vm.envAddress("ETH_ADDRESS_ON_TARA");
-//         console.log("ETH_ADDRESS_ON_TARA: %s", ethAddressOnTara);
-//     }
+        if (vm.envUint("PRIVATE_KEY") == 0) {
+            console.log("Skipping deployment because PRIVATE_KEY is not set");
+            return;
+        }
+        // check if balance is at least 2 * MINIMUM_CONNECTOR_DEPOSIT
+        if (address(deployerAddress).balance < (2 * REGISTRATION_FEE_TARA)) {
+            console.log(
+                "Skipping deployment because balance is less than 2 * MINIMUM_CONNECTOR_DEPOSIT + REGISTRATION_FEE_ETH"
+            );
+            return;
+        }
 
-//     function deployBLC() public {
-//         uint64 _slot = uint64(vm.envUint("SLOT"));
-//         uint64 _proposer_index = uint64(vm.envUint("PROPOSER_INDEX"));
-//         bytes32 _parent_root = vm.envBytes32("PARENT_ROOT");
-//         bytes32 _state_root = vm.envBytes32("STATE_ROOT");
-//         bytes32 _body_root = vm.envBytes32("BODY_ROOT");
-//         uint256 _block_number = vm.envUint("BLOCK_NUMBER");
-//         bytes32 _merkle_root = vm.envBytes32("MERKLE_ROOT");
-//         bytes32 _sync_root = vm.envBytes32("SYNC_COMMITTEE_ROOT");
-//         bytes32 _genesis_validators_root = vm.envBytes32("GENESIS_ROOT");
+        taraAddressOnEth = vm.envAddress("TARA_ADDRESS_ON_ETH");
+        console.log("TARA_ADDRESS_ON_ETH: %s", taraAddressOnEth);
+        ethAddressOnTara = vm.envAddress("ETH_ADDRESS_ON_TARA");
+        console.log("ETH_ADDRESS_ON_TARA: %s", ethAddressOnTara);
+    }
 
-//         // Deploy BeaconLightClient
+    function deployBLC() public {
+        uint64 _slot = uint64(vm.envUint("SLOT"));
+        uint64 _proposer_index = uint64(vm.envUint("PROPOSER_INDEX"));
+        bytes32 _parent_root = vm.envBytes32("PARENT_ROOT");
+        bytes32 _state_root = vm.envBytes32("STATE_ROOT");
+        bytes32 _body_root = vm.envBytes32("BODY_ROOT");
+        uint256 _block_number = vm.envUint("BLOCK_NUMBER");
+        bytes32 _merkle_root = vm.envBytes32("MERKLE_ROOT");
+        bytes32 _sync_root = vm.envBytes32("SYNC_COMMITTEE_ROOT");
+        bytes32 _genesis_validators_root = vm.envBytes32("GENESIS_ROOT");
 
-//         console.log("Period: %s", _slot / 32 / 256);
-//         beaconClient = new BeaconLightClient(
-//             _slot,
-//             _proposer_index,
-//             _parent_root,
-//             _state_root,
-//             _body_root,
-//             _block_number,
-//             _merkle_root,
-//             _sync_root,
-//             _genesis_validators_root
-//         );
+        // Deploy BeaconLightClient
 
-//         console.log("BeaconLightClient.sol address: %s", address(beaconClient));
-//     }
+        console.log("Period: %s", _slot / 32 / 256);
+        beaconClient = new BeaconLightClient(
+            _slot,
+            _proposer_index,
+            _parent_root,
+            _state_root,
+            _body_root,
+            _block_number,
+            _merkle_root,
+            _sync_root,
+            _genesis_validators_root
+        );
 
-//     function deployEthClient() public returns (address ethClientProxy) {
-//         address ethBridgeAddress = vm.envAddress("ETH_BRIDGE_ADDRESS");
-//         ethClientProxy = Upgrades.deployUUPSProxy(
-//             "EthClient.sol", abi.encodeCall(EthClient.initialize, (beaconClient, ethBridgeAddress))
-//         );
+        console.log("BeaconLightClient.sol address: %s", address(beaconClient));
+    }
 
-//         console.log("EthClient.sol proxy address: %s", ethClientProxy);
-//         address ethClientImpl = Upgrades.getImplementationAddress(ethClientProxy);
-//         console.log("EthClient.sol implementation address: %s", ethClientImpl);
-//         return ethClientProxy;
-//     }
+    function deployEthClient() public returns (address ethClientProxy) {
+        address ethBridgeAddress = vm.envAddress("ETH_BRIDGE_ADDRESS");
+        ethClientProxy = Upgrades.deployUUPSProxy(
+            "EthClient.sol", abi.encodeCall(EthClient.initialize, (beaconClient, ethBridgeAddress))
+        );
 
-//     function deployTaraBridge(address _ethClientProxy, uint256 _finalizationInterval)
-//         public
-//         returns (address taraBrigdeProxy)
-//     {
-//         taraBrigdeProxy = Upgrades.deployUUPSProxy(
-//             "TaraBridge.sol",
-//             abi.encodeCall(
-//                 TaraBridge.initialize,
-//                 (IBridgeLightClient(_ethClientProxy), _finalizationInterval)
-//             )
-//         );
+        console.log("EthClient.sol proxy address: %s", ethClientProxy);
+        address ethClientImpl = Upgrades.getImplementationAddress(ethClientProxy);
+        console.log("EthClient.sol implementation address: %s", ethClientImpl);
+        return ethClientProxy;
+    }
 
-//         console.log("TaraBridge.sol proxy address: %s", taraBrigdeProxy);
-//         console.log("TaraBridge.sol implementation address: %s", Upgrades.getImplementationAddress(taraBrigdeProxy));
-//         return taraBrigdeProxy;
-//     }
+    function deployTaraBridge(address _ethClientProxy, uint256 _finalizationInterval)
+        public
+        returns (address taraBrigdeProxy)
+    {
+        taraBrigdeProxy = Upgrades.deployUUPSProxy(
+            "TaraBridge.sol",
+            abi.encodeCall(
+                TaraBridge.initialize,
+                (
+                    IBridgeLightClient(_ethClientProxy),
+                    _finalizationInterval,
+                    FEE_MULTIPLIER_TARA,
+                    REGISTRATION_FEE_TARA,
+                    SETTLEMENT_FEE_TARA
+                )
+            )
+        );
 
-//     function run() public {
-//         vm.startBroadcast(deployerPrivateKey);
+        console.log("TaraBridge.sol proxy address: %s", taraBrigdeProxy);
+        console.log("TaraBridge.sol implementation address: %s", Upgrades.getImplementationAddress(taraBrigdeProxy));
+        return taraBrigdeProxy;
+    }
 
-//         deployBLC();
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
 
-//         address ethClientProxy = deployEthClient();
+        deployBLC();
 
-//         uint256 finalizationInterval = 100;
+        address ethClientProxy = deployEthClient();
 
-//         address taraBrigdeProxy = deployTaraBridge(ethClientProxy, finalizationInterval);
+        uint256 finalizationInterval = 100;
 
-//         address taraConnectorProxy = Upgrades.deployUUPSProxy(
-//             "NativeConnector.sol", abi.encodeCall(NativeConnector.initialize, (taraBrigdeProxy, taraAddressOnEth))
-//         );
+        address payable taraBrigdeProxy = payable(deployTaraBridge(ethClientProxy, finalizationInterval));
 
-//         // Fund TaraConnector with 2 ETH
-//         (bool success,) = payable(taraConnectorProxy).call{value: 2 ether}("");
+        address taraConnectorProxy = Upgrades.deployUUPSProxy(
+            "NativeConnector.sol",
+            abi.encodeCall(NativeConnector.initialize, (TaraBridge(taraBrigdeProxy), taraAddressOnEth))
+        );
 
-//         if (!success) {
-//             revert("Failed to fund the TaraConnector");
-//         }
+        // Fund TaraConnector with 2 ETH
+        (bool success,) = payable(taraConnectorProxy).call{value: 2 ether}("");
 
-//         console.log("NativeConnector.sol proxy address: %s", taraConnectorProxy);
-//         console.log(
-//             "NativeConnector.sol implementation address: %s", Upgrades.getImplementationAddress(taraConnectorProxy)
-//         );
+        if (!success) {
+            revert("Failed to fund the TaraConnector");
+        }
 
-//         // Initialize TaraConnector
-//         TaraBridge(taraBrigdeProxy).registerContract(IBridgeConnector(taraConnectorProxy));
+        console.log("NativeConnector.sol proxy address: %s", taraConnectorProxy);
+        console.log(
+            "NativeConnector.sol implementation address: %s", Upgrades.getImplementationAddress(taraConnectorProxy)
+        );
 
-//         address ethMintingConnectorProxy = Upgrades.deployUUPSProxy(
-//             "ERC20MintingConnector.sol",
-//             abi.encodeCall(
-//                 ERC20MintingConnector.initialize,
-//                 (address(taraBrigdeProxy), TestERC20(ethAddressOnTara), Constants.NATIVE_TOKEN_ADDRESS)
-//             )
-//         );
+        // Initialize TaraConnector
+        TaraBridge(taraBrigdeProxy).registerContract{value: REGISTRATION_FEE_TARA}(IBridgeConnector(taraConnectorProxy));
 
-//         console.log("ERC20MintingConnector.sol proxy address: %s", ethMintingConnectorProxy);
-//         console.log(
-//             "ERC20MintingConnector.sol implementation address: %s",
-//             Upgrades.getImplementationAddress(ethMintingConnectorProxy)
-//         );
+        address ethMintingConnectorProxy = Upgrades.deployUUPSProxy(
+            "ERC20MintingConnector.sol",
+            abi.encodeCall(
+                ERC20MintingConnector.initialize,
+                (TaraBridge(taraBrigdeProxy), TestERC20(ethAddressOnTara), Constants.NATIVE_TOKEN_ADDRESS)
+            )
+        );
 
-//         // Fund TaraConnector with 2 ETH
-//         (bool success2,) = payable(ethMintingConnectorProxy).call{value: 2 ether}("");
+        console.log("ERC20MintingConnector.sol proxy address: %s", ethMintingConnectorProxy);
+        console.log(
+            "ERC20MintingConnector.sol implementation address: %s",
+            Upgrades.getImplementationAddress(ethMintingConnectorProxy)
+        );
 
-//         if (!success2) {
-//             revert("Failed to fund the ethMintingConnectorProxy");
-//         }
+        // Initialize EthMintingConnectorProxy
+        TaraBridge(taraBrigdeProxy).registerContract{value: REGISTRATION_FEE_TARA}(
+            IBridgeConnector(ethMintingConnectorProxy)
+        );
 
-//         // Initialize EthMintingConnectorProxy
-//         TaraBridge(taraBrigdeProxy).registerContract(IBridgeConnector(ethMintingConnectorProxy));
+        // give ownership of erc20 to the connector
+        TestERC20(ethAddressOnTara).transferOwnership(ethMintingConnectorProxy);
 
-//         // give ownership of erc20 to the connector
-//         TestERC20(ethAddressOnTara).transferOwnership(ethMintingConnectorProxy);
+        console.log("TaraBridge initialized");
 
-//         console.log("TaraBridge initialized");
-
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.stopBroadcast();
+    }
+}
