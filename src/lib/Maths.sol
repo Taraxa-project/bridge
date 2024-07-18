c905118c952d260a7da4b98be17711b94ea11254
kstdl <kostello.d@gmail.com>
2024-07-18 15:49:45 +0200

    fix(TOB-TARA-2): Maths.add function edge case with int256.min

--- src/lib/Maths.sol
+++ src/lib/Maths.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.17;
 library Maths {
     function add(uint256 a, int256 b) internal pure returns (uint256) {
         if (b < 0) {
+            if (b == type(int256).min) {
+                return a - uint256(b);
+            }
             return a - uint256(-b);
         }
         return a + uint256(b);
