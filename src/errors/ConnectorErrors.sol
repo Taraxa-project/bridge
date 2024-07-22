e394eaca35ff672b4576bc290bddd5afb5d2d034
kstdl <kostello.d@gmail.com>
2024-07-22 10:58:51 +0200

    feat: optimize bridge.finalizeEpoch

--- src/errors/ConnectorErrors.sol
+++ src/errors/ConnectorErrors.sol
@@ -4,5 +4,4 @@ pragma solidity ^0.8.17;
 error InvalidEpoch(uint256 expected, uint256 actual);
 error NotBridge(address sender);
 error NoFinalizedState();
-error StateIsNotEmpty();
 error ZeroValueCall();
